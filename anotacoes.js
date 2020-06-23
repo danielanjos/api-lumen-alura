f6149acdf8f222eeb5ccf581cdd71e7879d703ce
Daniel Anjos <dmartins.anj@gmail.com>
2020-06-22 22:04:19 -0300

    First Commit

---
+++ anotacoes.js
@@ -0,0 +1,13 @@
+/**
+ * criar migration
+ * php artisan make:migration criar_tabela_series --create=series
+ *
+ * rodar migrations
+ * php artisan migrate
+ *
+ * criar seeder
+ * php artisan make:seeder UsuarioSeeder
+ *
+ * rodar seed
+ * php artisan db:seed
+ */
---
+++ app/Serie.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Serie extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['nome'];
+    protected $perPage = 10;
+    protected $appends = ['links'];
+
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class);
+    }
+
+    public function getLinksAttribute(): array
+    {
+        return [
+            'self' => '/api/series/' . $this->id,
+            'episodios' => '/api/series/' . $this->id . '/episodios'
+        ];
+    }
+}
